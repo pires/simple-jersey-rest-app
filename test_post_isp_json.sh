77a435394f6d689da0817a879430b143db24eadd
Paulo Pires <pjpires@gmail.com>
2012-05-22 12:49:50 +0100

    Added a script to test JSON upload to app.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

---
+++ test_post_isp_json.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# curl must be available, otherwise this script can't run.
+command -v curl >/dev/null 2>&1 || { echo "I require curl but it's not installed. Aborting." >&2; exit 1; }
+
+# send JSON
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"isp":{"id":1,"name":"Meo"}}' http://192.168.11.102:8080/jersey_rest/rest/admin/isp/
